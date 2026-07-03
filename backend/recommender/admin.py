72a9c56fc79d152d6d950b9525e4d6a74247257a
Ashim <pas080bct008@wrc.edu.np>
2026-07-03 13:38:03 +0545

    Fix admin configuration and update recommender

--- backend/recommender/admin.py
+++ backend/recommender/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
 # Register your models here.
+from .models import Book, Wishlist, ReadBook
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "isbn")
+    search_fields = ("title", "isbn")
+
+
+admin.site.register(Wishlist)
+admin.site.register(ReadBook)
